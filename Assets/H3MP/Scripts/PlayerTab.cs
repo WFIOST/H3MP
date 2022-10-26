216490bc51eeb08f274c8363bc2d8664274be33c
KacperObara <kacper.filip.obara@gmail.com>
2022-10-26 20:45:42 +0200

    Players List Panel

---
+++ Assets/H3MP/Scripts/PlayerTab.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerTab : MonoBehaviour 
+{
+	public Text NameText;
+	public Text PingText;
+	
+	public Text KickText;
+	public Text BanText;
+
+	private bool _confirmKick;
+	private bool _confirmBan;
+
+	private PlayersListPanel _panel;
+	
+	public void Initialize(PlayersListPanel panel)
+	{
+		// if (IsYou)
+		// 	Destroy(gameObject);
+
+		_panel = panel;
+
+		NameText.text = "Player #1";
+		PingText.text = "420ms";
+	}
+	
+	public void KickClicked()
+	{
+		if (_confirmKick)
+		{
+			//TODO Kick
+			_panel.RemovePlayerTab(this);
+			_panel.ResetConfirms();
+		}
+		else
+		{
+			_confirmKick = true;
+			KickText.text = "Confirm";
+		}
+	}
+
+	public void BanClicked()
+	{
+		if (_confirmBan)
+		{
+			//TODO Ban
+			_panel.RemovePlayerTab(this);
+			_panel.ResetConfirms();
+		}
+		else
+		{
+			_confirmBan = true;
+			BanText.text = "Confirm";
+		}
+	}
+
+	public void ResetConfirm()
+	{
+		_confirmKick = false;
+		_confirmBan = false;
+		
+		KickText.text = "Kick";
+		BanText.text = "Ban";
+	}
+}
---
+++ Assets/H3MP/Scripts/PlayersListPanel.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayersListPanel : MonoBehaviour
+{
+	public List<PlayerTab> PlayerTabs;
+	public PlayerTab PlayerTabPrefab;
+	public Transform PlayersListParent;
+
+	public static int PlayerCount = 4;
+
+	public Text StopServerText;
+	private bool _stopServerConfirm;
+	
+	public Text LobbyCountText;
+	
+	private void Start()
+	{
+		// Remove preview tabs
+		for (int i = PlayerTabs.Count - 1; i >= 0; i--)
+		{
+			RemovePlayerTab(PlayerTabs[i]);
+		}
+		
+		// Simulate players joining
+		for (int i = 0; i < PlayerCount; i++)
+		{
+			AddPlayerTab();
+		}
+	}
+
+	public void AddPlayerTab()
+	{
+		PlayerTab newTab = Instantiate(PlayerTabPrefab, PlayersListParent);
+		newTab.Initialize(this);
+		PlayerTabs.Add(newTab);
+		UpdateLobbyCount();
+	}
+
+	public void RemovePlayerTab(PlayerTab tab)
+	{
+		PlayerTabs.Remove(tab);
+		Destroy(tab.gameObject);
+		UpdateLobbyCount();
+	}
+
+	public void StopServerClicked()
+	{
+		if (_stopServerConfirm)
+		{
+			//TODO Stop server
+			ResetConfirms();
+		}
+		else
+		{
+			_stopServerConfirm = true;
+			StopServerText.text = "Confirm";
+		}
+	}
+	
+	public void ResetConfirms()
+	{
+		foreach (var tab in PlayerTabs)
+		{
+			tab.ResetConfirm();
+		}
+		
+		StopServerText.text = "Stop Server";
+	}
+	
+	private void UpdateLobbyCount()
+	{
+		LobbyCountText.text = PlayerTabs.Count + "/" + PlayerCount + " Players";
+	}
+}
