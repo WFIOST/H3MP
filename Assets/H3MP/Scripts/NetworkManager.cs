d3bbb6ba9078835b1c1f0c7523512e5bae3b4345
Frityet <ambhogal01@gmail.com>
2022-10-30 22:28:16 -0700

    Work on obejct sync

--- Assets/H3MP/Scripts/NetworkManager.cs
+++ Assets/H3MP/Scripts/NetworkManager.cs
@@ -6,57 +6,81 @@ using Riptide;
 using H3MP.Core;
 using System.Linq;
 using FistVR;
+using H3MP.Scripts;
+
 public class NetworkManager : MonoBehaviour
 {
     public static Action PlayerConnectedEvent;
-    
+
     public List<ScenePlayer> scenePlayers = new List<ScenePlayer>();
     public GameObject PlayerPrefab;
     public List<string> PlayerList;
     public static NetworkManager instance;
+
+    private Plugin _networking
+    {
+        get
+        {
+            if (Plugin.Instance == null) throw new Exception("Plugin is null? How can this be?");
+            return Plugin.Instance;
+        }
+    }
+
     private Player playerUpdateStruct;
-    private bool hasConnected = false;
-    private bool hasUpdatedID = false;
+    private bool hasConnected;
+    private bool hasUpdatedID;
+
+    private Dictionary<int, NetworkedBehaviour> _objects;
+
     // Use this for initialization
-    void Awake() {
-        
+    void Awake()
+    {
         DontDestroyOnLoad(gameObject);
         playerUpdateStruct = new Player();
         instance = this;
-       
-        // Plugin.Instance.Client.Send(Message.Create());
-        
-        Plugin.Instance.ClientMessageHandlers[(ushort)MessageIdentifier.Networking.PLAYER_LIST] = HandlePlayerListMessages; 
-        Plugin.Instance.ClientMessageHandlers[(ushort)MessageIdentifier.Player.UPDATE_TRANSFORM] = HandlePlayerMovementMessage;
-        Plugin.Instance.ClientMessageHandlers[(ushort)MessageIdentifier.Player.ENTER] = HandleConnectionInformationPacketMessage;
-        Plugin.Instance.ClientMessageHandlers[(ushort)MessageIdentifier.Networking.SYNC] = HandleSyncMessage;
+        _objects = new Dictionary<int, NetworkedBehaviour>();
+        _tmpIDMap = new Dictionary<int, NetworkedBehaviour>();
+
+        _networking.ClientMessageHandlers[(ushort) MessageIdentifier.Networking.PLAYER_LIST] = HandlePlayerListMessages;
+        _networking.ClientMessageHandlers[(ushort) MessageIdentifier.Player.UPDATE_TRANSFORM] =
+            HandlePlayerMovementMessage;
+        _networking.ClientMessageHandlers[(ushort) MessageIdentifier.Player.ENTER] =
+            HandleConnectionInformationPacketMessage;
+
+        _networking.ClientMessageHandlers[(ushort) MessageIdentifier.Object.BIRTH] = OnObjectBirth;
+        _networking.ClientMessageHandlers[(ushort) MessageIdentifier.Object.ID_SET] = TempIDToRealID;
+        _networking.ClientMessageHandlers[(ushort) MessageIdentifier.Object.DEATH] = OnDeathProclamation;
     }
 
 
     void Update()
-    {      
-       playerUpdateStruct.Head.Position = GM.CurrentPlayerBody.Head.position;
-       playerUpdateStruct.Head.Rotation = GM.CurrentPlayerBody.Head.rotation;
-       playerUpdateStruct.LeftHand.Position = GM.CurrentMovementManager.Hands[0].transform.position;
-       playerUpdateStruct.LeftHand.Rotation = GM.CurrentMovementManager.Hands[0].transform.rotation;
-       playerUpdateStruct.RightHand.Position = GM.CurrentMovementManager.Hands[1].transform.position;
-       playerUpdateStruct.RightHand.Rotation = GM.CurrentMovementManager.Hands[1].transform.rotation;
-        if (!hasUpdatedID && Plugin.Instance.Client.IsConnected)
+    {
+        playerUpdateStruct.Head.Position = GM.CurrentPlayerBody.Head.position;
+        playerUpdateStruct.Head.Rotation = GM.CurrentPlayerBody.Head.rotation;
+        playerUpdateStruct.LeftHand.Position = GM.CurrentMovementManager.Hands[0].transform.position;
+        playerUpdateStruct.LeftHand.Rotation = GM.CurrentMovementManager.Hands[0].transform.rotation;
+        playerUpdateStruct.RightHand.Position = GM.CurrentMovementManager.Hands[1].transform.position;
+        playerUpdateStruct.RightHand.Rotation = GM.CurrentMovementManager.Hands[1].transform.rotation;
+        if (!hasUpdatedID && _networking.Client.IsConnected)
         {
-            playerUpdateStruct.ID = Plugin.Instance.Client.Id;
-            playerUpdateStruct.Username = Plugin.Instance.Username.Value;
+            playerUpdateStruct.ID = _networking.Client.Id;
+            playerUpdateStruct.Username = _networking.Username.Value;
             hasUpdatedID = true;
         }
     }
 
     public void FixedUpdate()
     {
-        if (Plugin.Instance.Client.IsConnected)
+        if (_networking.Client.IsConnected)
         {
             PlayerMoveMessageSender();
         }
     }
-    public virtual void AddPlayer(ushort id, string username) {
+
+#region Players
+
+    public virtual void AddPlayer(ushort id, string username)
+    {
         GameObject Jeff = Instantiate(PlayerPrefab);
         ScenePlayer JeffSP = Jeff.GetComponent<ScenePlayer>();
         scenePlayers.Add(JeffSP);
@@ -64,32 +88,11 @@ public class NetworkManager : MonoBehaviour
         JeffSP.ID = id;
         JeffSP.Name = username;
         JeffSP.namePlate.text = username;
-        
+
         if (PlayerConnectedEvent != null)
             PlayerConnectedEvent.Invoke();
     }
-    public virtual void ScoreBoardUpdate()
-    { 
-        //this will be used to update the networkingPanel's scoreboard UI
-    }
-    public virtual void ScoreBoardUpdate(ushort ID)
-    {
 
-        for (int i = 0; i < scenePlayers.Count; i++)
-        {
-            if (scenePlayers[i].ID == ID)
-            {
-                PlayerList.RemoveAt(i);
-                break;
-            }
-        }
-        foreach (ScenePlayer PS in scenePlayers)
-        {
-            PS.ScoreBoardPosition = scenePlayers.IndexOf(PS);
-        }
-
-    }
-   
     public void HandlePlayerListMessages(Message message)
     {
         Player[] Players = message.GetSerializables<Player>();
@@ -98,25 +101,23 @@ public class NetworkManager : MonoBehaviour
             AddPlayer(player.ID, player.Username);
         }
     }
+
     //Handles Handles the message at connection where the server sends the client a list of currently connected players
     public void HandleConnectionInformationPacketMessage(Message message)
     {
         AddPlayer(message.GetUShort(), message.GetString());
         hasConnected = true;
     }
-    public void HandleSyncMessage(Message message)
-    { 
-    
-    }
-        //Handles incoming player mocement packets
+
+    //Handles incoming player mocement packets
     public void HandlePlayerMovementMessage(Message message)
     {
 
         Player NewMovePacket = new Player();
-       
-       NewMovePacket.Deserialize(message);
+
+        NewMovePacket.Deserialize(message);
         // Debug.Log("Move Packet ID " + NewMovePacket.ID.ToString());
-        for (int i = 0; i <= scenePlayers.Count-1; i++)
+        for (int i = 0; i <= scenePlayers.Count - 1; i++)
         {
             if (scenePlayers[i].ID == NewMovePacket.ID)
             {
@@ -124,13 +125,63 @@ public class NetworkManager : MonoBehaviour
             }
         }
     }
+
     private void PlayerMoveMessageSender()
     {
-        Message msg = Message.Create(MessageSendMode.Unreliable, (ushort)MessageIdentifier.Player.UPDATE_TRANSFORM);
-       
+        Message msg = Message.Create(MessageSendMode.Unreliable, (ushort) MessageIdentifier.Player.UPDATE_TRANSFORM);
+
         msg.Add(playerUpdateStruct);
-        Plugin.Instance.Client.Send(msg);
+        _networking.Client.Send(msg);
 
     }
-    
+
+#endregion
+
+
+#region Objects
+
+    private int _tempIDReference;
+    private Dictionary<int, NetworkedBehaviour> _tmpIDMap;
+
+    public void RegisterObject(NetworkedBehaviour obj)
+    {
+        _tmpIDMap[_tempIDReference] = obj;
+        _networking.Client.Send(Message.Create(sendMode: MessageSendMode.Reliable, id: MessageIdentifier.Object.BIRTH)
+                                       .Add(_tempIDReference)
+                                       .Add(obj.Data));
+        _tempIDReference++;
+    }
+
+    public void MurderObject(int id)
+    {
+        _networking.Client.Send(Message.Create(sendMode: MessageSendMode.Reliable, id: MessageIdentifier.Object.DEATH)
+                                       .Add(id));
+        KillObj(id);
+    }
+
+    private void TempIDToRealID(Message msg)
+    {
+        int tmp = msg.GetInt(), real = msg.GetInt();
+        _tmpIDMap[tmp].SetID(real);
+        _objects[real] = _tmpIDMap[tmp];
+        _tmpIDMap.Remove(tmp);
+    }
+
+    private void OnDeathProclamation(Message msg)
+    {
+        KillObj(id: msg.GetInt());
+    }
+
+    private void KillObj(int id)
+    {
+        _objects[id].Die();
+        _objects.Remove(id);
+    }
+
+
+    private void OnObjectBirth(Message msg)
+    {
+        
+    }
+#endregion
 }
--- Assets/H3MP/Scripts/NetworkedBehaviour.cs
+++ Assets/H3MP/Scripts/NetworkedBehaviour.cs
@@ -8,7 +8,43 @@ namespace H3MP.Scripts
     {
         public NetworkedObject Data;
 
+        private Rigidbody _rigidbody;
+
         private void Awake()
+        {
+            var rb = GetComponent<Rigidbody>();
+            if (rb == null) rb = GetComponentInChildren<Rigidbody>();
+            if (rb == null)
+            {
+                Destroy(this);
+                throw new Exception("No rigidbody found on object " + gameObject.name);
+            }
+            
+            _rigidbody = rb;
+            
+            SetData();
+            
+            NetworkManager.instance.RegisterObject(this);
+        }
+
+        private void FixedUpdate()
+        {
+            
+        }
+
+        private void SetData()
+        {
+            Data.Transform.Position = transform.position;
+            Data.Transform.Rotation = transform.rotation;
+            Data.Velocity = _rigidbody.velocity;
+        }
+        
+        public void SetID(int id)
+        {
+            Data.ID = id;
+        }
+
+        public void Die()
         {
             
         }
